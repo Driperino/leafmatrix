ed67ba38bdcbfba63e1c0686d4a18574b1c99ac2
Vincent <vincent.winkler98@gmail.com>
2024-12-13 16:50:38 -0800

    working on auth

--- leafmatrix/src/app/page.tsx
+++ leafmatrix/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold mb-4">Welcome to LeafMatrix</h1>
         <Button className="mt-4">Login</Button>
       </div>
